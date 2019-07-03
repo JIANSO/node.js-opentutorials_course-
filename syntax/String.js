5df14772d19ee34ac571ebeb8fcae45118c29b9c
JIANSO <dothewillof@naver.com>
2019-07-03 18:37:47 +0900

    2019.07.03 오후 6시 37분

---
+++ syntax/String.js
@@ -0,0 +1,9 @@
+console.log("console.log(1+1); >" + 1+ 1);
+console.log("console.log('1'+'1'); >" + '1'+'1');
+
+
+console.log(
+'Lorem il, sunt in culpa qui off anim id est laborum'.length
+
+
+);
---
+++ syntax/variable2.js
@@ -0,0 +1,8 @@
+console.log('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.');
+
+var letter = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+console.log("use varialbe >> " + letter);
+
+
+// 중복을 제거하면 자연스럽게 좋은 프로그램이 됩니다.
